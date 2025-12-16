b7701e0c5b4718ce4ae86c4ba55fee77ba51eb1b
JJTB100 <jonathan@broster.co.uk>
2025-12-16 22:13:46 +0000

    stage2

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ int do_echo(char *args);
 int do_exit(char *args);
 int do_type(char *args);
 int do_pwd(char *args);
+int do_cd(char * args);
 
 // --- GLOBAL REGISTRY ---
 Builtin builtins[] = {
@@ -24,6 +25,7 @@ Builtin builtins[] = {
   {"exit", do_exit},
   {"type", do_type},
   {"pwd",  do_pwd},
+  {"cd", do_cd},
   {NULL, NULL} // Marks end
 };
 
@@ -37,7 +39,21 @@ int do_echo(char *args){
 int do_exit(char *args) {
   return -1; 
 }
-
+int do_cd(char * args){
+  char *path = args;
+  if (path == NULL || strcmp(path, "~" == 0)){
+    path = getenv("HOME");
+    if (path == NULL){
+      printf("cd: HOME not set\n");
+      return -1;
+    }
+  }
+  if (chdir(path) !=0){
+    printf("cd: %s: No such file or directory\n", path);
+    return -1;
+  }
+  return 0;
+}
 int do_pwd(char *args){
   char cwd[1024]; 
     // getcwd(buffer, size) fills the buffer with the current directory
