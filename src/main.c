b7f247f012b14141b0d3812a302505ca7de2ebd6
JJTB100 <jonathan@broster.co.uk>
2025-12-16 22:17:46 +0000

    stage2

--- src/main.c
+++ src/main.c
@@ -45,25 +45,25 @@ int do_cd(char * args){
     path = getenv("HOME");
     if (path == NULL){
       printf("cd: HOME not set\n");
-      return -1;
+      return 1;
     }
   }
   if (chdir(path) !=0){
     printf("cd: %s: No such file or directory\n", path);
-    return -1;
+    return 1;
   }
   return 0;
 }
 int do_pwd(char *args){
   char cwd[1024]; 
-    // getcwd(buffer, size) fills the buffer with the current directory
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
-    } else {
-        perror("pwd error");
-        return 1;
-    }
-    return 0;
+  // getcwd(buffer, size) fills the buffer with the current directory
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      printf("%s\n", cwd);
+  } else {
+      perror("pwd error");
+      return 1;
+  }
+  return 0;
 }
 int do_type(char *args) {
   if (args == NULL) return 1;
