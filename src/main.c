7063c30248ec383ac3ef3b890598c5bb26a818bc
JJTB100 <jonathan@broster.co.uk>
2025-12-16 23:30:39 +0000

    stage2

--- src/main.c
+++ src/main.c
@@ -6,18 +6,18 @@
 #include <unistd.h>
 
 // --- TYPE DEFINITIONS ---
-typedef int (*builtin_handler)(char *args);
+typedef int (*builtin_handler)(char **argv);
 typedef struct {
   const char *name;
   builtin_handler handler;
 } Builtin;
 
 // --- FUNCTION PROTOTYPES ---
-int do_echo(char *args);
-int do_exit(char *args);
-int do_type(char *args);
-int do_pwd(char *args);
-int do_cd(char * args);
+int do_echo(char **argv);
+int do_exit(char **argv);
+int do_type(char **argv);
+int do_pwd(char **argv);
+int do_cd(char **argv);
 
 // --- GLOBAL REGISTRY ---
 Builtin builtins[] = {
@@ -30,51 +30,63 @@ Builtin builtins[] = {
 };
 
 // --- IMPLEMENTATIONS ---
-int do_echo(char *args){
-  if(args) printf("%s\n", args);
-  else printf("\n");
+int do_echo(char **argv) {
+  for (int i = 1; argv[i] != NULL; i++) {
+    printf("%s", argv[i]);
+    if (argv[i + 1] != NULL) printf(" ");
+  }
+  printf("\n");
   return 0;
 }
 
-int do_exit(char *args) {
-  return -1; 
+int do_exit(char **argv) {
+  return -1;
 }
-int do_cd(char * args){
-  char *path = args;
-  if (path == NULL || strcmp(path, "~") == 0){
+
+int do_cd(char **argv) {
+  // argv[0] is "cd", argv[1] is the path
+  char *path = argv[1];
+  
+  if (path == NULL || strcmp(path, "~") == 0) {
     path = getenv("HOME");
-    if (path == NULL){
+    if (path == NULL) {
       printf("cd: HOME not set\n");
       return 1;
     }
   }
-  if (chdir(path) !=0){
+  
+  if (chdir(path) != 0) {
     printf("cd: %s: No such file or directory\n", path);
     return 1;
   }
   return 0;
 }
-int do_pwd(char *args){
-  char cwd[1024]; 
-  // getcwd(buffer, size) fills the buffer with the current directory
+
+int do_pwd(char **argv) {
+  char cwd[1024];
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-      printf("%s\n", cwd);
+    printf("%s\n", cwd);
   } else {
-      perror("pwd error");
-      return 1;
+    perror("pwd error");
+    return 1;
   }
   return 0;
 }
-int do_type(char *args) {
-  if (args == NULL) return 1;
 
+int do_type(char **argv) {
+  // argv[0] is "type", argv[1] is the command to check
+  if (argv[1] == NULL) return 1;
+  char *target = argv[1];
+
+  // Check Builtins
   for (int i = 0; builtins[i].name != NULL; i++) {
-    if (strcmp(args, builtins[i].name) == 0) {
-      printf("%s is a shell builtin\n", args);
+    if (strcmp(target, builtins[i].name) == 0) {
+      printf("%s is a shell builtin\n", target);
       return 0;
     }
   }
 
+  // Check PATH
   char *path_env = getenv("PATH");
   if (path_env == NULL) return 1;
 
@@ -84,100 +96,107 @@ int do_type(char *args) {
   int found = 0;
 
   while (dir != NULL) {
-    // Concatenate 'dir' + '/' + 'args' 
-    snprintf(full_path, sizeof(full_path), "%s/%s", dir, args);
-    // Check if that file exists using access()
+    snprintf(full_path, sizeof(full_path), "%s/%s", dir, target);
     if (access(full_path, X_OK) == 0) {
-      printf("%s is %s\n", args, full_path);
+      printf("%s is %s\n", target, full_path);
       found = 1;
       break;
     }
     dir = strtok(NULL, ":");
   }
-  if(!found){
-    printf("%s: not found\n", args);
-    return 0;
+  
+  if (!found) {
+    printf("%s: not found\n", target);
   }
+  
   free(path_copy);
-  return 1;
+  return 0;
+}
+
+int parse_input(char *input, char **argv){
+  int argc = 0;
+  int in_quotes = 0;
+  char * current_token = input;
+
+  // Loop through every character checking for a single quote
+  for (int i = 0; input[i] != '\0'; i++) {
+    if(input[i] == '\''){
+      in_quotes = !in_quotes;
+      // In place move all the characters left one
+      memmove(&input[i], &input[i+1], strlen(&input[i+1]) + 1);
+      i--;
+      continue;
+    }
+    if(input[i]==' ' && !in_quotes){
+      input[i] = '\0';
+      if (*current_token != '\0') {
+        argv[argc++] = current_token;
+      }
+      current_token = &input[i + 1];
+    }
+  }
+  if (*current_token != '\0') {
+    argv[argc++] = current_token;
+  }
+  
+  argv[argc] = NULL; // Null-terminate the list
+  return argc;
 }
 
 // --- MAIN LOOP ---
-int main(int argc, char *argv[]) {
-    // Disable output buffering
-    setbuf(stdout, NULL);
-    
-    char inp[1024];
-    
-    printf("$ ");
+int main(int argc, char *main_argv[]) {
+  setbuf(stdout, NULL);
+  printf("$ ");
 
-    while (fgets(inp, 1024, stdin) != NULL) {
-        // Clean up input (remove newline)
-        inp[strcspn(inp, "\n")] = '\0';
-        
-        // Parse Command vs Args
-        char *space_pos = strchr(inp, ' ');
-        char *command = NULL;
-        char *args = NULL;
+  char inp[1024];
 
-        if (space_pos != NULL){
-            *space_pos = '\0';
-            command = inp;
-            args = space_pos + 1;
-            while(*args == ' ') args++; 
-        } else {
-            command = inp;
-            args = NULL;
-        }
+  while (fgets(inp, 1024, stdin) != NULL) {
+    inp[strcspn(inp, "\n")] = '\0';
 
-        // Check Builtins
-        int found_builtin = 0; 
-        for (int i = 0; builtins[i].name != NULL; i++) {
-            if (strcmp(command, builtins[i].name) == 0) {
-                int result = builtins[i].handler(args);
-                if (result == -1) return 0; // Exit shell
-                found_builtin = 1;
-                break;
-            }
-        }
+    char *argv[100];
+    int argc = parse_input(inp, argv);
 
-        // Run External Program (if not a builtin)
-        if (!found_builtin) {
-            pid_t pid = fork();
-
-            if (pid == 0) {
-                // --- CHILD PROCESS ---
-                char *cmd_args[100];
-                cmd_args[0] = command;
-                int idx = 1;
-
-                // Split args for execvp
-                if (args != NULL) {
-                    char *token = strtok(args, " ");
-                    while (token != NULL) {
-                        cmd_args[idx++] = token;
-                        token = strtok(NULL, " ");
-                    }
-                }
-                cmd_args[idx] = NULL; // Null terminate the list
-
-                // execvp automatically searches PATH
-                execvp(command, cmd_args);
-
-                // If we get here, execvp failed (command not found)
-                printf("%s: command not found\n", command);
-                exit(1); 
-            } else if (pid > 0) {
-                // --- PARENT PROCESS ---
-                int status;
-                wait(&status);
-            } else {
-                perror("fork");
-            }
-        }
-        
-        printf("$ ");
+    if (argc == 0) {
+      printf("$ ");
+      continue;
     }
 
-    return 0;
+    char *command = argv[0];
+    int found_builtin = 0;
+
+    // Check Builtins
+    for (int i = 0; builtins[i].name != NULL; i++) {
+      if (strcmp(command, builtins[i].name) == 0) {
+        // Pass the entire argv array to the handler
+        int result = builtins[i].handler(argv);
+        if (result == -1) return 0; // Exit shell
+        found_builtin = 1;
+        break;
+      }
+    }
+
+    // Run External Program
+    if (!found_builtin) {
+      pid_t pid = fork();
+
+      if (pid == 0) {
+        // Child Process
+        // execvp accepts the argv array directly!
+        execvp(command, argv);
+        
+        printf("%s: command not found\n", command);
+        exit(1);
+      } else if (pid > 0) {
+        // Parent Process
+        int status;
+        wait(&status);
+      } else {
+        perror("fork");
+      }
+    }
+
+    printf("$ ");
+  }
+
+  return 0;
 }
