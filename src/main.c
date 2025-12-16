bdaf7ca9b4b31f6d7e5dcf815b191e4a020170c8
JJTB100 <jonathan@broster.co.uk>
2025-12-16 19:43:30 +0000

    stage2

--- src/main.c
+++ src/main.c
@@ -9,16 +9,32 @@ int main(int argc, char *argv[]) {
   printf("$ ");
 
   ssize_t max_size = 1024;
-  char command[max_size];
+  char inp[max_size];
 
-  while (fgets(command, max_size, stdin) != NULL){
+  while (fgets(inp, max_size, stdin) != NULL){
     // Remove trailing new line
-    command[strcspn(command, "\n")] = '\0';
+    inp[strcspn(inp, "\n")] = '\0';
+    char *space_pos = strchr(inp, ' ');
+    char *command = NULL;
+    char *args = NULL;
+
+    if (space_pos != NULL){
+      *space_pos = '\0';
+      command = inp;
+      args = space_pos +1;
+      while(*args == ' ') args++; //Skip leading whitespace
+    } else{
+      command = inp;
+      args = NULL;
+    }
 
     if (strcmp(command, "exit") == 0){
       return 0;
+    } else if (strcmp(command, "echo") == 0){
+      printf("%s\n", args);
+    } else{
+      printf("%s: command not found\n", inp);
     }
-    printf("%s: command not found\n", command);
     printf("$ ");
   }
 
