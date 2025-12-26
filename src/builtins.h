aec4bd84578852de794b101c87a23016be221826
JJTB100 <jonathan@broster.co.uk>
2025-12-26 12:27:00 +0000

    stage2

--- src/builtins.h
+++ src/builtins.h
@@ -23,5 +23,5 @@ int do_history(char **argv);
 // Helper to execute builtins with redirection
 int handle_builtin(char *command, char **argv, char *out_file, char *err_file, int append_out, int append_err);
 int last_line_saved;
-int count_file_lines(char *filename)
+int count_file_lines(const char *filename);
 #endif
